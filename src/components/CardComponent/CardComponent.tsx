3788ae70ee1d292be25ca4544e7a7268a114445a
NovikovIlya <novikov.i.sergeevich@gmail.com>
2024-03-27 00:51:55 +0300

    Поправил баги

--- src/components/CardComponent/CardComponent.tsx
+++ src/components/CardComponent/CardComponent.tsx
@@ -1,25 +1,22 @@
-import React from 'react'
-import styles from './CardComponent.module.css'
-import { Link, useNavigate } from 'react-router-dom'
-import BottomComponent from '../Bottom/BottomComponent';
-import LeftComponent from '../Left/LeftComponent';
-import { useAppSelector } from '../../hooks/redux';
-import TextComponent from '../Text/TextComponent';
-import Body from '../Body/Body';
+import styles from "./CardComponent.module.css";
+import BottomComponent from "../Bottom/BottomComponent";
+import LeftComponent from "../Left/LeftComponent";
+import TextComponent from "../Text/TextComponent";
+import Body from "../Body/Body";
 
 const CardComponent = () => {
-  const {step:isStep} = useAppSelector((state)=>state.sliceUrl)
-  const navigate = useNavigate();
+
   return (
     <div className={styles.container}>
-        <LeftComponent/>
-        <div className={styles.right}>
-              <TextComponent/>
-              <Body />
-              <BottomComponent />
-        </div>
-    </div>
-  )
-}
+      <LeftComponent />
 
-export default CardComponent
+      <div className={styles.right}>
+        <TextComponent />
+        <Body />
+        <BottomComponent />
+      </div>
+    </div>
+  );
+};
+
+export default CardComponent;
